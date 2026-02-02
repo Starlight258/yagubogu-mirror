f9a00a3c4a158c8b94af6811758799605678e000
medAndro <med4ndro@gmail.com>
2026-02-02 23:36:54 +0900

    feat: 통계 조회 시 연도가 변경되면 화면을 다시 그리도록 수정

--- android/app/src/main/java/com/yagubogu/ui/common/model/PieChartItemValue.kt
+++ android/app/src/main/java/com/yagubogu/ui/common/model/PieChartItemValue.kt
@@ -1,7 +1,6 @@
 package com.yagubogu.ui.common.model
 
 import androidx.compose.ui.graphics.Color
-import com.yagubogu.ui.theme.Primary500
 
 data class PieChartItemValue(
     val strokeColor: Color,
