322d0e1df9c4601db0122b9d62866ad22f88c334
jiyunlee <02leejiyun@naver.com>
2026-02-09 01:21:46 +0900

    refactor: LocalContext.getString 대신 LocalResources.getString을 사용하도록 수정

--- android/app/src/main/java/com/yagubogu/ui/setting/SettingMainScreen.kt
+++ android/app/src/main/java/com/yagubogu/ui/setting/SettingMainScreen.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
+import android.content.res.Resources
 import android.graphics.Bitmap
 import android.net.Uri
 import androidx.activity.ComponentActivity
@@ -32,6 +33,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalResources
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -102,10 +104,11 @@ fun SettingMainScreen(
         viewModel.fetchMemberInfo()
     }
 
+    val resources: Resources = LocalResources.current
     LaunchedEffect(settingEvent) {
         if (settingEvent is SettingEvent.NicknameEdit) {
             val message =
-                context.getString(
+                resources.getString(
                     R.string.setting_edited_nickname_alert,
                     (settingEvent as SettingEvent.NicknameEdit).newNickname,
                 )
