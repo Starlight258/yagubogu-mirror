155ed9d891f85d4a9c34cb954c56103ab1da37cb
jiyunlee <02leejiyun@naver.com>
2026-02-21 20:15:38 +0900

    build: 야구보구 v2.2.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.yagubogu"
         minSdk = 29
         targetSdk = 36
-        versionCode = 2_02_00
-        versionName = "2.2.0"
+        versionCode = 2_02_01
+        versionName = "2.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
