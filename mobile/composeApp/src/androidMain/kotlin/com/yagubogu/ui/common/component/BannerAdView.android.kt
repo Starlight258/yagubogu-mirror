fc3fe58589d3f19302054c3bd3a772a164e95481
ijh1298 <imjh0417@gmail.com>
2026-04-17 16:15:11 +0900

    fix: 배너 광고 AdView 생성 시 Activity 컨텍스트 명시 및 BannerAdEventCallback 등록

--- mobile/composeApp/src/androidMain/kotlin/com/yagubogu/ui/common/component/BannerAdView.android.kt
+++ mobile/composeApp/src/androidMain/kotlin/com/yagubogu/ui/common/component/BannerAdView.android.kt
@@ -1,13 +1,16 @@
 package com.yagubogu.ui.common.component
 
+import androidx.activity.compose.LocalActivity
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
 import com.google.android.libraries.ads.mobile.sdk.banner.AdSize as SdkAdSize
 import com.google.android.libraries.ads.mobile.sdk.banner.AdView
 import com.google.android.libraries.ads.mobile.sdk.banner.BannerAd
+import com.google.android.libraries.ads.mobile.sdk.banner.BannerAdEventCallback
 import com.google.android.libraries.ads.mobile.sdk.banner.BannerAdRequest
 import com.google.android.libraries.ads.mobile.sdk.common.AdLoadCallback
+import com.google.android.libraries.ads.mobile.sdk.common.FullScreenContentError
 import com.google.android.libraries.ads.mobile.sdk.common.LoadAdError
 
 private fun AdSize.toSdkAdSize(): SdkAdSize =
@@ -23,15 +26,28 @@ actual fun BannerAdView(
     adSize: AdSize,
     modifier: Modifier,
 ) {
+    val activity = LocalActivity.current ?: return
+
     AndroidView(
         modifier = modifier,
-        factory = { context ->
-            AdView(context).also { adView ->
+        factory = {
+            AdView(activity).also { adView ->
                 val adRequest = BannerAdRequest.Builder(adUnitId, adSize.toSdkAdSize()).build()
                 adView.loadAd(
                     adRequest,
                     object : AdLoadCallback<BannerAd> {
-                        override fun onAdLoaded(ad: BannerAd) = Unit
+                        override fun onAdLoaded(ad: BannerAd) {
+                            ad.adEventCallback = object : BannerAdEventCallback {
+                                override fun onAdImpression() = Unit
+                                override fun onAdClicked() = Unit
+                                override fun onAdShowedFullScreenContent() = Unit
+                                override fun onAdDismissedFullScreenContent() = Unit
+                                override fun onAdFailedToShowFullScreenContent(
+                                    fullScreenContentError: FullScreenContentError
+                                ) = Unit
+                            }
+                        }
+
                         override fun onAdFailedToLoad(adError: LoadAdError) = Unit
                     },
                 )
