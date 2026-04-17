c695be6b6280a497ec842122a0d3bbebf152f5af
ijh1298 <imjh0417@gmail.com>
2026-04-17 16:16:50 +0900

    fix: 전면 광고 Activity 참조를 LocalActivity로 교체

--- mobile/composeApp/src/androidMain/kotlin/com/yagubogu/ui/common/component/InterstitialAdEffect.android.kt
+++ mobile/composeApp/src/androidMain/kotlin/com/yagubogu/ui/common/component/InterstitialAdEffect.android.kt
@@ -1,6 +1,6 @@
 package com.yagubogu.ui.common.component
 
-import android.app.Activity
+import androidx.activity.compose.LocalActivity
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -8,7 +8,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.platform.LocalContext
 import com.google.android.libraries.ads.mobile.sdk.common.AdLoadCallback
 import com.google.android.libraries.ads.mobile.sdk.common.AdRequest
 import com.google.android.libraries.ads.mobile.sdk.common.FullScreenContentError
@@ -23,8 +22,7 @@ actual fun InterstitialAdEffect(
     adUnitId: String,
     onAdComplete: () -> Unit,
 ) {
-    val context = LocalContext.current
-    val activity = context as? Activity ?: return
+    val activity = LocalActivity.current ?: return
 
     var loadedAd by remember { mutableStateOf<InterstitialAd?>(null) }
     val currentOnAdComplete by rememberUpdatedState(onAdComplete)
