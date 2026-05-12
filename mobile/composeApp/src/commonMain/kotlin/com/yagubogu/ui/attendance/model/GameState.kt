8f493fb4b8303840f5de2c97256859af08ef21b3
jiyunlee <02leejiyun@naver.com>
2026-05-12 13:58:47 +0900

    refactor: `GameState` 모델에 `UNKNOWN` 상태 추가 및 파싱 로직 개선
    - `GameState` 열거형 클래스에 `UNKNOWN` 타입 추가
    - `GameState.from()` 메서드에서 일치하는 상태가 없을 경우 예외를 발생시키는 대신 `UNKNOWN`을 반환하도록 수정

--- mobile/composeApp/src/commonMain/kotlin/com/yagubogu/ui/attendance/model/GameState.kt
+++ mobile/composeApp/src/commonMain/kotlin/com/yagubogu/ui/attendance/model/GameState.kt
@@ -5,9 +5,10 @@ enum class GameState {
     LIVE, // 경기 중
     COMPLETED, // 경기 종료
     CANCELED, // 경기 취소
+    UNKNOWN, // 알 수 없는 상태
     ;
 
     companion object {
-        fun from(state: String): GameState = entries.find { it.name == state } ?: throw IllegalArgumentException()
+        fun from(state: String): GameState = entries.find { it.name == state } ?: UNKNOWN
     }
 }
