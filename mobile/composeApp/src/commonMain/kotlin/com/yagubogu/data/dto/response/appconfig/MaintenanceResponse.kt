f9bbf10a4db697bc10fe0f99384a406aa2a16a44
medAndro <med4ndro@gmail.com>
2026-05-12 14:19:48 +0900

    refactor: MaintenanceResponse DTO 기본값 수정 및 네트워크 안내 문구 추가

--- mobile/composeApp/src/commonMain/kotlin/com/yagubogu/data/dto/response/appconfig/MaintenanceResponse.kt
+++ mobile/composeApp/src/commonMain/kotlin/com/yagubogu/data/dto/response/appconfig/MaintenanceResponse.kt
@@ -6,15 +6,15 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class MaintenanceResponse(
     @SerialName("is_show")
-    override val isShow: Boolean = false, // 점검중 팝업 표시 여부
+    override val isShow: Boolean = true, // 점검중 팝업 표시 여부
     @SerialName("id")
     override val id: Int = -1, // 점검 메시지 id
     @SerialName("emoji")
     override val emoji: String? = null, // 다이얼로그 이모지
     @SerialName("title")
-    override val title: String? = null, // 다이얼로그 제목
+    override val title: String? = "인터넷에 연결 없음", // 다이얼로그 제목
     @SerialName("message")
-    override val message: String? = null, // 다이얼로그 메시지
+    override val message: String? = "야구보구를 사용하기 위해서\n데이터 또는 와이파이 연결이 필요합니다", // 다이얼로그 메시지
     @SerialName("skippable_days")
     override val skippableDays: Int? = null, // 스킵 가능한 일 수
     @SerialName("is_login_block")
